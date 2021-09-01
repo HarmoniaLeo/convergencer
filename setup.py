000130316f83842c5f957524313f75eaeab48174
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2021-09-01 16:55:28 +0800

    0.1

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+ 
+setup(name='convergencer',
+      version="0.1",
+      description='Data science workflow of automatic data reading, preprocessing, model selection, model integration, model parameter adjustment and effect evaluation.',
+      author='HarmoniaLeo',
+      author_email='1532282692@qq.com',
+      maintainer='HarmoniaLeo',
+      maintainer_email='1532282692@qq.com',
+      packages=['convergencer'],
+      license="Public domain",
+      platforms=["any"],
+     )
